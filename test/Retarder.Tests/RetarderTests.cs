ed474a3ebfd5e3311055065e3cd6a87a6084f9d7
luiscalcada <luispedrocalcada@gmail.com>
2016-07-25 23:21:37 +0100

    Added "Retarder.Tests" project and added some tests.

---
+++ test/Retarder.Tests/RetarderTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+using awes0mecoderz.Retarder;
+
+namespace awes0mecoderz.Retarder.Tests
+{
+    public class RetarderTests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            var retarder = new Retarder(10, 10);
+            Assert.True(retarder.IsTrue());
+
+        }
+    }
+}
