6771d3704cfe7427bb7e9a0b072fc452a14b7d52
luiscalcada <luispedrocalcada@gmail.com>
2016-08-03 23:22:06 +0100

    Fix of fix.

--- build.sh
+++ build.sh
@@ -13,14 +13,14 @@ dotnet restore
 
 # Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
 # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
+# dotnet test ./test/Retarder.Tests -c Release -f netcoreapp1.0
+# # Instead, run directly with mono for the full .net version 
+# dotnet build ./test/Retarder.Tests -c Release -f net451
 
-dotnet test ./test/Retarder.Tests -c Release -f netcoreapp1.0
+dotnet test ./test/Retarder.Tests -c Release
 
-# Instead, run directly with mono for the full .net version 
-dotnet build ./test/Retarder.Tests -c Release -f net451
-
-mono ./test/Retarder.Tests/bin/Release/net451/*/dotnet-test-xunit.exe
-mono ./test/Retarder.Tests/bin/Release/net451/*/Retarder.Tests.dll
+# mono ./test/Retarder.Tests/bin/Release/net451/*/dotnet-test-xunit.exe
+# mono ./test/Retarder.Tests/bin/Release/net451/*/Retarder.Tests.dll
 
 revision=${TRAVIS_JOB_ID:=1}  
 revision=$(printf "%04d" $revision) 
